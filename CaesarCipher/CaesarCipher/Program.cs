e7593952b0eb8ba850efa59ae5ab85e19d39b484
user888736 <szymonkwasniewski@protonmail.com>
2021-10-14 21:58:18 +0200

    config test 2

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -14,6 +14,7 @@ namespace CaesarCipher
                 {
                     input[i] = (int)input[i] + shift;
                     sdsds
+                        sdsad
                 }
             }
 
