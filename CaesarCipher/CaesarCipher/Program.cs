be0fad464dd79b79ecab05b2ddd72258199696f5
user888736 <szymonkwasniewski@protonmail.com>
2021-10-14 22:43:55 +0200

    sss

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -7,14 +7,14 @@ namespace CaesarCipher
         static void Main(string[] args)
         {
             string input = "Abcd";
+            
             int shift = 4;
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i] >= 65 && input[i] <= 90 - shift)
                 {
                     input[i] = (int)input[i] + shift;
-                    sdsds
-                        sdsad
+                    
                 }
             }
 
