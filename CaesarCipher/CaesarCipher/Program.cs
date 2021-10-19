f644d803e02445dc7f2a96e891042a97d5db63a8
user888736 <szymonkwasniewski@protonmail.com>
2021-10-19 19:14:36 +0200

    Final encrypt solution

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CaesarCipher
 {
@@ -6,18 +7,49 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            string input = "Abcd";
-            
-            int shift = 4;
-            for (int i = 0; i < input.Length; i++)
+            Console.WriteLine("Podaj tekst, ktory chcesz zaszyfrowac: ");
+            string input = Console.ReadLine();
+            char[] inputArr = new char[input.Length];
+            Console.WriteLine("Podaj skok z jakim ma być wykonane szyfrowanie: ");
+            int shift = Convert.ToInt32(Console.ReadLine());
+
+            for(int i =0; i<input.Length; i++)
             {
-                if (input[i] >= 65 && input[i] <= 90 - shift)
-                {
-                    input[i] = (int)input[i] + shift;
-                    
-                }
+                inputArr[i] = input[i];
             }
 
+            List<char> encryptedInput = new List<char>();
+
+            if (shift > 26)
+            {
+                Console.WriteLine("Podany skok jest za duży!");  
+            }
+            else
+            {
+                foreach (int j in inputArr)
+                {
+                    int c = j;
+                    if (j >= 65 && j <= 90 - shift)
+                    {
+                        c += shift;
+                    }
+                    else if (j >= 91 - shift && j <= 90)
+                    {
+                        c = c - 26 + shift;
+                    }
+                    else if (j >= 97 && j < 122 - shift)
+                    {
+                        c += shift;
+                    }
+                    else if (j >= 123 - shift && j <= 122)
+                    {
+                        c = c - 26 + shift;
+                    }
+                    encryptedInput.Add((char)c);
+                }
+            }
+            string output = string.Join("",encryptedInput.ToArray());
+            Console.WriteLine(output);
         }
     }
 }
