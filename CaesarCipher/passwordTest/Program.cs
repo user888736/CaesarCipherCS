3e207d8e00450f601eb52132914c8311a772b3ff
user888736 <szymonkwasniewski@protonmail.com>
2021-10-20 19:25:32 +0200

    Password strength program added

--- CaesarCipher/passwordTest/Program.cs
+++ CaesarCipher/passwordTest/Program.cs
@@ -1,16 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
 namespace passwordTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj proszę hasło, które chcesz sprawdzić: ");
-            string pass = Console.ReadLine();
-            List<string> passSummary = new List<string>();
+            string path = @"D:\gitRepos\CaesarCipherCS\password.txt";
+            string password = File.ReadAllText(path);
+            
+            
+            int score = 0;
 
+            if (password.Length>=8)
+                score++;
+            
+            if (password.Length >= 12)
+                score++;
 
+            if (Regex.IsMatch(password, @"[0-9]+(\.[0-9][0-9]?)?", RegexOptions.ECMAScript))
+                score++; 
+
+            if (Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z]).+$", RegexOptions.ECMAScript))
+                score++;
+
+            if (Regex.IsMatch(password, @"[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]", RegexOptions.ECMAScript))
+                score++;
+
+            if(score == 0)
+                Console.WriteLine("Nie podałeś hasła.");
+            else if(score == 1)
+                Console.WriteLine("Hasło jest bardzo słabe!");
+            else if (score == 2)
+                Console.WriteLine("Hasło jest słabe.");
+            else if(score==3)
+                Console.WriteLine("Hasło wymaga poprawy!");
+            else if(score == 4)
+                Console.WriteLine("Hasło jest silne.");
+            else if(score==5)
+                Console.WriteLine("Hasło jest bardzo silne.");
         }
     }
 }
