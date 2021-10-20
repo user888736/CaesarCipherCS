485b1d0f910b70ea04ec565e986bc1dcea62a93e
user888736 <szymonkwasniewski@protonmail.com>
2021-10-20 18:07:33 +0200

    Starting new password test project

---
+++ CaesarCipher/passwordTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace passwordTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj proszę hasło, które chcesz sprawdzić: ");
+            string pass = Console.ReadLine();
+        }
+    }
+}
